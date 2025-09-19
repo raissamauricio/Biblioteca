4a0359422bdc18cc266a57c61f5a93b9bfc79c8c
raissamauricio <raissaamauriciooo@gmail.com>
2025-09-19 15:44:05 -0300

    adicionado delete show e update

--- app/Http/Controllers/AutorController.php
+++ app/Http/Controllers/AutorController.php
@@ -20,4 +20,61 @@ class AutorController extends Controller
         return response()->json($autor);
     }
 
+    public function show($id) {
+           $autor = Autor::find($id);
+
+        //verifica se a tarefa existe ou se a variavel tarefa é nula
+        if ($autor == null) {
+            return response()->json([
+                'erro' => 'autor não encontrado['
+            ]);
+        }
+    }
+
+    public function update(Request $request) {
+        $autor = Autor::find($request->id);
+
+        if ($autor == null) {
+            return response()->json([
+                'erro' => 'autor não encontardo'
+            ]);
+        }
+
+        if (isset($request->nome_completo)) {
+            $autor->nome_completo = $request->nome_completo;
+        }
+
+     
+        if (isset($request->data_nascimento)) {
+            $autor->data_nascimento = $request->data_nascimento;
+        }
+
+       
+        if (isset($request->nacionalidade)) {
+            $autor->nacionalidade = $request->nacionalidade;
+        }
+
+        $autor->update();
+
+        return response()->json([
+            'mensagem' => 'atualizada'
+        ]);
+    }
+
+    public function delete($id) {
+         $autor = Autor::find($id);
+        
+        if ($autor == null) {
+            return response()->json([
+                'erro' => 'autor não encontrado'
+            ]);
+       }
+
+       $autor->delete();
+
+       return response()->json([
+        'mensagem' => 'excluido'
+       ]);
+    }
+
 }
--- app/Http/Controllers/EmprestimoController.php
+++ app/Http/Controllers/EmprestimoController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Emprestimo;
 use Illuminate\Http\Request;
 
 class EmprestimoController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $emprestimo = Emprestimo::create([
+            'data_empretimo' => $request -> data_empretimo,
+            'data_devolucao' => $request -> data_devolucao,
+            'codigo_membro' => $request -> codigo_membro,
+            'codigo_livro' => $request -> codigo_livro
+        ]);
+        return response()->json($emprestimo);
+    }
+
+    public function delete($id) {
+        $emprestimo = Emprestimo::find($id);
+        
+        if ($emprestimo == null) {
+            return response()->json([
+                'erro' => 'emprestimo não encontrado'
+            ]);
+       }
+
+       $emprestimo->delete();
+
+       return response()->json([
+        'mensagem' => 'excluido'
+       ]);
+    }
+
+    public function update() {
+         return response()->json([
+                'erro' => 'emprestimo não encontardo'
+            ]);
+        
+
+        if (isset($request->data_empretimo)) {
+            $emprestimo->data_empretimo = $request->data_empretimo;
+        }
+
+     
+        if (isset($request->data_devolucao)) {
+            $emprestimo->data_devolucao = $request->data_devolucao;
+        }
+
+       
+        if (isset($request->codigo_membro)) {
+            $emprestimo->codigo_membro = $request->codigo_membro;
+        }
+
+        $emprestimo->update();
+
+        return response()->json([
+            'mensagem' => 'atualizada'
+        ]);
+    }
+
+    public function index() {}
 }
